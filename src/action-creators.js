4e4645b051d0b61755840905e895f3df6af1283f
Ryan Tablada <ryan.tablada@gmail.com>
2017-03-10 14:27:30 -0600

    Finish initial spike

--- src/action-creators.js
+++ src/action-creators.js
@@ -15,6 +15,11 @@ const parseResponse = (r) => {
   return Promise.reject(r.json());
 };
 
+const headers = {
+  Accept: 'application/json',
+  'Content-Type': 'application/json',
+};
+
 export default function createActionCreators(resourceName, options) {
   const actionNames = createActionNames(resourceName);
 
@@ -35,8 +40,7 @@ export default function createActionCreators(resourceName, options) {
       .then(parseResponse)
       .then((data) => {
         dispatch(actions.findAllSuccess(data));
-      })
-      .catch((err) => {
+      }).catch((err) => {
         dispatch(actions.findAllError(err));
       });
   };
@@ -50,10 +54,58 @@ export default function createActionCreators(resourceName, options) {
       .then(parseResponse)
       .then((data) => {
         dispatch(actions.findOneSuccess(data));
-      })
-      .catch((err) => {
+      }).catch((err) => {
         dispatch(actions.findOneError(err));
       });
+  };
+
+  actions.create = formData => (dispatch) => {
+    dispatch(actions.createStart());
+
+    const baseUrl = options.createUrl || options.url;
+
+    return fetch(baseUrl, {
+      method: 'POST',
+      headers,
+      body: JSON.stringify(formData),
+    }).then(parseResponse)
+      .then((data) => {
+        dispatch(actions.createSuccess(data));
+      }).catch((err) => {
+        dispatch(actions.createError(err));
+      });
+  };
+
+  actions.update = (id, formData) => (dispatch) => {
+    dispatch(actions.updateStart());
+
+    const baseUrl = options.updateUrl || options.url;
+
+    return fetch(`${baseUrl}/${id}`, {
+      method: 'PUT',
+      headers,
+      body: JSON.stringify(formData),
+    }).then(parseResponse)
+      .then((data) => {
+        dispatch(actions.updateSuccess(data));
+      }).catch((err) => {
+        dispatch(actions.updateError(err));
+      });
+  };
+
+  actions.detstroy = id => (dispatch) => {
+    dispatch(actions.detstroyStart());
+
+    const baseUrl = options.detstroyUrl || options.url;
+
+    return fetch(`${baseUrl}/${id}`, {
+      method: 'DELETE',
+      headers,
+    }).then((data) => {
+      dispatch(actions.detstroySuccess(data));
+    }).catch((err) => {
+      dispatch(actions.detstroyError(err));
+    });
   };
 
   return actions;
---
+++ src/reducer.js
@@ -0,0 +1,63 @@
+import { combineReducers } from 'redux';
+import unionBy from 'lodash.unionBy';
+import createActionNames from './action-names';
+
+export default function createReducer(resourceName, options) {
+  const actions = createActionNames(resourceName);
+
+  const customReducers = options.reducers || {};
+  const loadingCustomReducers = customReducers.loading || {};
+  const itemsCustomReducers = customReducers.items || {};
+
+  return combineReducers({
+    loading: (state = null, action) => {
+      if (loadingCustomReducers[action.type]) {
+        return loadingCustomReducers[action.type](state, action);
+      }
+
+      switch (action.type) {
+        case actions.findAllStart:
+        case actions.findOneStart:
+        case actions.createStart:
+        case actions.updateStart:
+        case actions.destroyStart:
+          return 'loading';
+        case actions.findAllSuccess:
+        case actions.findOneSuccess:
+        case actions.createSuccess:
+        case actions.updateSuccess:
+        case actions.destroySuccess:
+          return null;
+        case actions.findAllError:
+        case actions.findOneError:
+        case actions.createError:
+        case actions.updateError:
+        case actions.destroyError:
+          return 'error';
+        default:
+          return state;
+      }
+    },
+
+    items: (state = [], action) {
+      if (itemsCustomReducers[action.type]) {
+        return itemsCustomReducers[action.type](state, action);
+      }
+
+      switch (action.type) {
+        case actions.findAllSuccess:
+          return unionBy(action.data, state, 'id');
+        case actions.findOneSuccess:
+          return unionBy([action.data], state, 'id');
+        case actions.createSuccess:
+          return unionBy([action.data], state, 'id');
+        case actions.updateSuccess:
+          return unionBy([action.data], state, 'id');
+        case actions.destroySuccess:
+          return state.filter((x) => x.id === action.id);
+        default:
+          return state;
+      }
+    }
+  });
+}
