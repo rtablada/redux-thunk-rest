5d01b7aa16d2e340d53317863648d20b525b979b
Ryan Tablada <ryan.tablada@gmail.com>
2017-03-10 14:28:12 -0600

    Update createResource

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import createActionNames from './action-names';
 import createActionCreators from './action-creators';
+import createReducer from './reducer';
 
 export default function (resourceName, options) {
   return {
     actions: createActionNames(resourceName),
     actionCreators: createActionCreators(resourceName, options),
+    reducer: createReducer(resourceName, options),
   };
 }
